2ef9750a5adb89152216f03cb10c999bcd66caf0
alexandrefernandesjs <alexandrefernandesjs@gmail.com>
2021-02-03 02:34:57 +0000

    Novo modelos adicionados

--- domain/model/bank.go
+++ domain/model/bank.go
@@ -1,35 +1,39 @@
 package model
 
 import (
-	// "github.com/asaskevich/govalidator"
+	"github.com/asaskevich/govalidator"
 	uuid "github.com/satori/go.uuid"
 	"time"
 )
 
-type Bank struct{
-	Base 
-	Code string `json:"code"`
-	Name string `json:"name"`
-} 
+type Bank struct {
+	Base     `valid:"required"`
+	Code     string     `json:"code" valid:"notnull"`
+	Name     string     `json:"name" valid:"notnull"`
+	Accounts []*Account `valid:"-"`
+}
 
-func (bank *Bank) isValid() error{
+func (bank *Bank) isValid() error {
+	_, err := govalidator.ValidateStruct(bank)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
-func NewBank(code.string, name.string)(*Bank, error){
+func NewBank(code string, name string) (*Bank, error) {
 	bank := Bank{
-		Code: code, 
+		Code: code,
 		Name: name,
 	}
 
-	bank.ID = uuid.NewV4.String()
+	bank.ID = uuid.NewV4().String()
 	bank.CreatedAt = time.Now()
 
 	err := bank.isValid()
-	if err != nil{
+	if err != nil {
 		return nil, err
 	}
 
-	return  &bank null
+	return &bank, nil
 }
-
